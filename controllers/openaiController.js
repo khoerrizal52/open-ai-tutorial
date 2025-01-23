0d84b4b2519edf7e8e4b8f27a44d0a84b1491055
Khoerurrizal <kuroi.nichirin@gmail.com>
2025-01-23 17:56:34 +0700

    generate topics in a list

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -2,20 +2,22 @@ const openai = require('../config/openaiConfig')
 const { zodResponseFormat } = require("openai/helpers/zod");
 const { z } = require("zod");
 
-const CalendarEvent = z.object({
-    name: z.string(),
-    date: z.string(),
-    participants: z.array(z.string()),
+const Topics = z.object({
+    topic: z.array(z.string()),
 });
 
 const generateMeta = async (title) => {
     const description = await openai.chat.completions.create({
         model: "gpt-4o-2024-08-06",
         messages: [
-            { role: "system", content: "Extract the event information." },
-            { role: "user", content: "Alice and Bob are going to a science fair on Friday." },
+            { role: "system", content: "Provide array of simple string" },
+            {
+                role: 'user',
+                content: `give me 5 simple topics to talk in a list`
+            }
         ],
-        response_format: zodResponseFormat(CalendarEvent, "event"),
+        response_format: zodResponseFormat(Topics, "topics"),
+        max_tokens: 100
     })
 
     console.log(description.choices[0].message.content)
